823d32fc8c1a6bfcdbbc8066c81749137f0fd762
Lucas Giebler <42948021+Viper4@users.noreply.github.com>
2024-09-27 16:30:06 -0500

    Floor Trap
    -Created Trap Floor that breaks when player walks over it and plays particles and audio

---
+++ Assets/Scripts/TrapFloor.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapFloor : MonoBehaviour
+{
+    [SerializeField] GameObject breakParticles;
+    AudioSource breakAudio;
+
+    private void Start()
+    {
+        TryGetComponent<AudioSource>(out breakAudio);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            GameObject newParticles = Instantiate(breakParticles, transform.position, transform.rotation);
+            newParticles.transform.eulerAngles = new Vector3(90, newParticles.transform.eulerAngles.y, newParticles.transform.eulerAngles.z);
+            ParticleSystem particleSystem = newParticles.GetComponent<ParticleSystem>();
+            var shape = particleSystem.shape;
+            shape.radius = transform.localScale.x * 0.5f;
+            particleSystem.Play();
+            if(breakAudio != null)
+                breakAudio.Play();
+            Destroy(particleSystem.gameObject, 5);
+            Destroy(gameObject);
+        }
+    }
+}
